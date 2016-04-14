b9c57c7dddb3835a9317969a86cf5ed0851062b9
Alicia Broadhurst <abroadhurst@cnm.edu>
2016-04-14 11:46:53 -0600

    Vetted list of entities and attributes, as well as relations in brainstorm.

--- index.php
+++ index.php
@@ -1,9 +1,15 @@
 <!DOCTYPE html>
 <html lang="eng">
 	<head>
-		
+		<meta charset="UTF-8">
+		<title>Data Design Project</title>
 	</head>
 	<body>
+		<header>
+			<h1>Data Design Project: Steam Game Storefront Review Section</h1>
+		</header>
+		<main>
 		
+		</main>
 	</body>
 </html> 
