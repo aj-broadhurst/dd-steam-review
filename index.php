f1f044af6eda678fcff3aa0c1edbe2f141311e27
Alicia Broadhurst <abroadhurst@cnm.edu>
2016-04-14 12:01:49 -0600

    Begin transfer from brainstorm.txt to index.php, committing before lunch

--- index.php
+++ index.php
@@ -9,7 +9,42 @@
 			<h1>Data Design Project: Steam Game Storefront Review Section</h1>
 		</header>
 		<main>
-		
+		<h2>Backend Setup</h2>
+			<h3>Attributes and Entities</h3>
+			<h4>Game</h4>
+			<ul>
+				<li>gameID</li>
+				<li>gameName</li>
+			</ul>
+			<h4>User</h4>
+			<ul>
+				<li>userID</li>
+			<li>userName</li>
+			<li>userLevel</li>
+			<li>userProducts</li>
+			<li>userReviews</li>
+			<li>userEmail</li>
+			<li>userSalt</li>
+			<li>userHash</li>
+			</ul>
+			<h4>Review</h4>
+			<ul>
+				<li>reviewID</li>
+				<li>reviewText</li>
+				<li>reviewDate</li>
+				<li>reviewRecommendation</li>
+				<li>userID</li>
+				<li>gameID</li>
+			</ul>>
+			Comment
+			commentID
+			commentText
+			commentDate
+			userID
+			Rating
+			ratingHelpfulness
+			userID
+			reviewID
 		</main>
 	</body>
 </html> 
