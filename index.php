def0af57ec72027d482681baf6eefe09bb09f272
Alicia Broadhurst <abroadhurst@cnm.edu>
2016-04-15 12:35:30 -0600

    Changed case on id attributes, changed header tags around

--- index.php
+++ index.php
@@ -11,8 +11,9 @@
 			<p>Phase 1 for the Data Design Project</p>
 		</header>
 		<main>
-		<h2>Frontend</h2>
-			<h3>Persona</h3>
+		<h2>Phase 1 Assignment</h2>
+		<h3>Frontend</h3>
+			<h4>Persona</h4>
 			<p><b>Name</b>: Alexander Powell<br>
 				<b>Age</b>: 20<br>
 				<b>Profession</b>: Alexander is a full-time student at the University of Colorado in Colorado Springs.  He is majoring in Game Design and Development with a minor in Visual Arts and is currently a sophomore.  He also works part time at one of the local Starbucks to help pay for school and other expenses.<br>
@@ -20,7 +21,7 @@
 				<b>Attitudes and Behaviors</b>: Because he is a student, and as is the typical trend for Steam users, Alexander tends to make most of his purchases during Steam sales.  Alexander does follow games journalism, blogs, etcetera, and thus usually knows about upcoming titles, especially in his favorite genres or by his favorite developers.  He eagerly anticipates specific titles before release, and will often purchase these titles before any sort of major price drop.  For titles that he is less familiar with, the content on the games' store page is usually the largest deciding factor.<br>
 				<b>Frustrations and Needs</b>: Alexander uses game storefronts to finalize impulse purchase and fully understand his purchases.  Alexander needs to be able to use the reviews to make an informed decision about games he is very familiar with, or games he knows very little about.<br>
 				<b>Goals</b>: Alex wants to know a few things, is he getting the most bang for his buck? And will he enjoy playing this game? He likes to find other users who know what they are talking about, and who play the same kind of games that he likes.</p>
-			<h3>Use Cases</h3>
+			<h4>Use Cases</h4>
 			<p><b>Use Case 1:</b>Text</p>
 			<ol>
 				<li>text</li>
@@ -29,16 +30,16 @@
 			<ol>
 				<li>Text</li>
 			</ol>
-		<h2>Backend</h2>
-			<h3>Attributes and Entities</h3>
-			<h4>Game</h4>
+		<h3>Backend</h3>
+			<h4>Attributes and Entities</h4>
+			<h5>Game</h5>
 			<ul>
-				<li>gameID</li>
+				<li>gameId</li>
 				<li>gameName</li>
 			</ul>
-			<h4>User</h4>
+			<h5>User</h5>
 			<ul>
-				<li>userID</li>
+				<li>userId</li>
 			<li>userName</li>
 			<li>userLevel</li>
 			<li>userProducts</li>
@@ -47,29 +48,30 @@
 			<li>userSalt</li>
 			<li>userHash</li>
 			</ul>
-			<h4>Review</h4>
+			<h5>Review</h5>
 			<ul>
-				<li>reviewID</li>
+				<li>reviewId</li>
 				<li>reviewText</li>
 				<li>reviewDate</li>
 				<li>reviewRecommendation</li>
-				<li>userID</li>
-				<li>gameID</li>
+				<li>userId</li>
+				<li>gameId</li>
 			</ul>
-			<h4>Comment</h4>
+			<h5>Comment</h5>
 				<ul>
-					<li>commentID</li>
+					<li>commentId</li>
 					<li>commentText</li>
 					<li>commentDate</li>
-					<li>userID</li>
+					<li>userId</li>
 				</ul>
-			<h4>Rating</h4>
+			<h5>Rating</h5>
 				<ul>
 					<li>ratingHelpfulness</li>
-					<li>userID</li>
-					<li>reviewID</li>
+					<li>userId</li>
+					<li>reviewId</li>
 				</ul>
-			<h3>Relations</h3>
+			<h4>Relations</h4>
+			<h2>Phase 2 Assignment</h2>
 		</main>
 	</body>
 </html> 
