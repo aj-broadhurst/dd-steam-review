f5293a873609e7530e20c2faee2da1e5949db9fe
Alicia Broadhurst <abroadhurst@cnm.edu>
2016-04-16 12:32:47 -0600

    Added use cases.

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="eng">
+<html lang="en">
 	<head>
 		<meta charset="UTF-8">
 		<title>Data Design Project</title>
@@ -8,6 +8,7 @@
 		<header>
 			<h1>Data Design Project:</h1>
 			<h2> Steam Game Storefront Review Section</h2>
+			<p><i>Alicia Broadhurst</i></p>
 			<p>Phase 1 for the Data Design Project</p>
 		</header>
 		<main>
@@ -22,13 +23,28 @@
 				<b>Frustrations and Needs</b>: Alexander uses game storefronts to finalize impulse purchase and fully understand his purchases.  Alexander needs to be able to use the reviews to make an informed decision about games he is very familiar with, or games he knows very little about.<br>
 				<b>Goals</b>: Alex wants to know a few things, is he getting the most bang for his buck? And will he enjoy playing this game? He likes to find other users who know what they are talking about, and who play the same kind of games that he likes.</p>
 			<h4>Use Cases</h4>
-			<p><b>Use Case 1:</b>Text</p>
+			<p><b>Use Case 1: </b>Goal - User wants to write a review:</p>
 			<ol>
-				<li>text</li>
+				<li>Alexander accesses the Steam homepage at <a href = "https://store.steampowered.com">https://store.steampowered.com</a></li>
+				<li>Alexander logs in, by clicking 'login' in the very top right corner.
+				<li>On the following screen, Alexander enters his username and password.  His SteamGuard settings are set up to send a verification code to the Steam app on his phone. Once he accesses that, and has typed the code into the pop-up prompt on the website, he is logged in and returned to the 'Featured Items' page.</li>
+				<li>Alexander searches for the requested game, in this case 'Rocket League'. The search box is in the upper right hand corner. As he types, games that match appear in a drop down list below the search bar.  He selects 'Rocket League' from the top of the list. He is taken to the 'Rocket League' storefront.</li>
+				<li>Since Alexander owns 'Rocket League' the storefront notes just below the video and summary elements that 'Rocket League is already in your Steam Library' and offers options to either 'Install Steam', 'Play Now' or Write a Review with an accompanying text box.</li>
+				<li>Alexander writes the content of his review in the text box, and selects a recommendation: either 'Yes' or 'No'.  In this case, 'Yes'.</li>
+				<li>Alexander clicks on 'Post review' and his review is submitted to the game's review database.</li>
 			</ol>
-			<p><b>Use Case 2:</b>Text</p>
+			<p><b>Use Case 2: </b>Goal - User wants to read/interact with reviews:</p>
 			<ol>
-				<li>Text</li>
+				<li>Alexander is browsing the featured items on the Steam homepage at <a href = "https://store.steampowered.com">https://store.steampowered.com</a> </li>
+				<li>Since Alexander is already logged in, he has all the rights granted to a Steam user, and doesn't need to log-in again.</li>
+				<li>He recalls a game he heard about a few months back, and believes it was recently released. He types what he remembers of the title into the search bar in the upper right hand corner of the page. 'Hyper Light'</li>
+				<li>Because he isn't sure on the full title, he presses 'Enter' instead of selecting a game from the drop-down list, in order to get a full look at all the possible titles. 'Hyper Light Drifer' is the first title in the list, the only title containing those words, and the icon looks correct; therefore Alexander selects it and proceeds to the storefront.</li>
+				<li>He watches the trailer, reads the description, and then moves onto the reviews by scrolling towards the bottom of the game's storefront.</li>
+				<li>The reviews are initially sorted by helpfulness, with most helpful reviews being displayed first.</li>
+				<li>Alexander scrolls down the page, reading reviews as he goes.  Once he reaches the end, he clicks on 'Load More Reviews' to have more reviews loaded on the page.</li>
+				<li>He decides that the second review was most helpful, and that one of the later reviews was rather useless, and scrolls back up to rate them.</li>
+				<li>Under the body of each review is a query asking 'Was this review helpful?' and three corresponding prompts. 'Yes', 'No', and 'Funny'. For the reviews he wanted to rate, Alexander clicks on the appropriate button. These buttons are now highlighted in a slightly lighter blue, while the text has become white, instead of blue, to indicate the interaction.</li>
+				<li>Alexander is done reading reviews for now, and while he is leaning towards purchasing the game; he is still making up his mind.</li>
 			</ol>
 		<h3>Backend</h3>
 			<h4>Attributes and Entities</h4>
